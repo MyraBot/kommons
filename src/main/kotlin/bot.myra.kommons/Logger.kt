a0c5b54f6cb0a0c4537faced7ac7cecc1fa52e9e
marian <elias-marian@web.de>
2022-08-07 13:04:09 +0200

    :zap: Added logger factory caching

--- src/main/kotlin/bot.myra.kommons/Logger.kt
+++ src/main/kotlin/bot.myra.kommons/Logger.kt
@@ -1,19 +1,23 @@
 package bot.myra.kommons
 
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.reflect.KClass
 
-suspend fun kInfo(klass: KClass<*>, message: suspend (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).info(message.invoke(Unit).toString())
-fun info(klass: KClass<*>, message: (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).info(message.invoke(Unit).toString())
+private val loggers = mutableMapOf<KClass<*>, Logger>()
+private fun getLogger(klass: KClass<*>) = loggers.getOrPut(klass) { LoggerFactory.getLogger(klass.java) }
 
-suspend fun kError(klass: KClass<*>, message: suspend (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).error(message.invoke(Unit).toString())
-fun error(klass: KClass<*>, message: (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).error(message.invoke(Unit).toString())
+suspend fun kInfo(klass: KClass<*>, message: suspend (Unit) -> Any?) = getLogger(klass).info(message.invoke(Unit).toString())
+fun info(klass: KClass<*>, message: (Unit) -> Any?) = getLogger(klass).info(message.invoke(Unit).toString())
 
-suspend fun kWarn(klass: KClass<*>, message: suspend (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).warn(message.invoke(Unit).toString())
-fun warn(klass: KClass<*>, message: (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).warn(message.invoke(Unit).toString())
+suspend fun kError(klass: KClass<*>, message: suspend (Unit) -> Any?) = getLogger(klass).error(message.invoke(Unit).toString())
+fun error(klass: KClass<*>, message: (Unit) -> Any?) = getLogger(klass).error(message.invoke(Unit).toString())
 
-suspend fun kDebug(klass: KClass<*>, message: suspend (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).debug(message.invoke(Unit).toString())
-fun debug(klass: KClass<*>, message: (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).debug(message.invoke(Unit).toString())
+suspend fun kWarn(klass: KClass<*>, message: suspend (Unit) -> Any?) = getLogger(klass).warn(message.invoke(Unit).toString())
+fun warn(klass: KClass<*>, message: (Unit) -> Any?) = getLogger(klass).warn(message.invoke(Unit).toString())
 
-suspend fun kTrace(klass: KClass<*>, message: suspend (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).trace(message.invoke(Unit).toString())
-fun trace(klass: KClass<*>, message: (Unit) -> Any?) = LoggerFactory.getLogger(klass.java).trace(message.invoke(Unit).toString())
+suspend fun kDebug(klass: KClass<*>, message: suspend (Unit) -> Any?) = getLogger(klass).debug(message.invoke(Unit).toString())
+fun debug(klass: KClass<*>, message: (Unit) -> Any?) = getLogger(klass).debug(message.invoke(Unit).toString())
+
+suspend fun kTrace(klass: KClass<*>, message: suspend (Unit) -> Any?) = getLogger(klass).trace(message.invoke(Unit).toString())
+fun trace(klass: KClass<*>, message: (Unit) -> Any?) = getLogger(klass).trace(message.invoke(Unit).toString())
